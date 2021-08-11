ed381b06e3af89b506102f96cfdd56fe5bddc6ca
Karla Nieves <karlaxnieves@gmail.com>
2021-08-11 14:37:35 -0700

    checkSchemeId is done

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const db = require('../../data/db-config')
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -7,7 +9,22 @@
   }
 */
 const checkSchemeId = (req, res, next) => {
-
+  try {
+    const existing = await db('schemes')
+    .where('scheme_id', req.params.scheme_id)
+    .first()
+    
+    if(!existing) {
+      next({
+        status: 404,
+        message: `scheme with scheme_id ${req.params.scheme_id} not found`
+      })
+    } else {
+      next()
+    }
+    } catch(err){
+    next(err)
+    }
 }
 
 /*
